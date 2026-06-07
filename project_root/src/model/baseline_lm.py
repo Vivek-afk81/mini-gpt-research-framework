de745273518b4596e83960262a645e858fca21c0
Vivek-afk81 <vivekchauhan560999@gmail.com>
2026-06-07 22:24:57 +0530

    tracking shapes

--- project_root/src/model/baseline_lm.py
+++ project_root/src/model/baseline_lm.py
@@ -24,18 +24,29 @@ class BaselineLM(nn.Module):
 
     def forward(self, input_ids):
 
+        print("Input IDs:",input_ids.shape)
         B, T = input_ids.shape
+
 
         positions = torch.arange(T, device=input_ids.device)
         pos_embeddings = self.pos_emb(positions)
 
+        print("Pos Emb:",pos_embeddings.shape)
+
         token_embeddings = self.token_emb(input_ids)
+        print("Token Emb:",token_embeddings.shape)
 
         x = token_embeddings + pos_embeddings
+        print("After Add:",x.shape)
 
         # NEW: transformer block
         x = self.block(x)
+        print("After Block:",x.shape)
 
         logits = self.lm_head(x)
+        print("Logits:",logits.shape)
+        print("Logits min:", logits.min().item())
+        print("Logits max:", logits.max().item())
+        print("Logits mean:", logits.mean().item())
 
         return logits
